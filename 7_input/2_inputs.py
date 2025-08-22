8476288942082d140ebe847bebafb0e1214bd8e1
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2025-08-22 08:02:51 -0300

    iniciando estudos sobre inputs

---
+++ 7_input/2_inputs.py
@@ -0,0 +1,3 @@
+# Assim como posso usar funções integradas diretamente nos inputs, como vimos
+# no documento anterior com "float(input('...'))" também posso, diretamente
+# neles, usar métodos, ou ainda usá-los como condições para if ou while:
